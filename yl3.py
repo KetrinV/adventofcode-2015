4361cb3a7d0d85bc3a732863e4b801a23116cfba
KetrnV <ketrnvanatoa@gmail.com>
2022-11-16 10:26:17 +0200

    -

--- yl3.py
+++ yl3.py
@@ -1,5 +1,27 @@
-from __future__ import print_function
-
-verbose = False
-
 f = open('input3.txt')
+
+def increment_house(x, y, houses):
+    if (x, y) in houses:
+        houses[(x, y)] += 1
+    else:
+        houses[(x, y)] = 1
+
+def calc_move(x, y, move):
+    if move == '^':
+        y += 1
+    elif move == 'v':
+        y -= 1
+    elif move == '>':
+        x += 1
+    else:
+        x -= 1
+    return x, y
+
+santaX = 0
+santaY = 0
+houses = {}
+increment_house(santaX, santaY, houses)
+for char in data:
+    santaX, santaY = calc_move(santaX, santaY, char)
+    increment_house(santaX, santaY, houses)
+print(len(houses))
